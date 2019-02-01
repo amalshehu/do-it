8d732dce733273628d04a2e12b94d8d2ed8654be
Amal Shehu <amalshehu@gmail.com>
2019-02-02 03:07:57 +0530

    Add Todos component

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,8 +1,14 @@
-<template></template>
+<template>
+  <div>
+    <h1>Todos</h1>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  name: "Todos"
+};
 </script>
 
-<style>
+<style scoped>
 </style>
