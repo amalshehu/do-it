8d1a0383c467b18a079a65ec679be372aa67c7e8
Amal Shehu <amalshehu@gmail.com>
2019-02-02 03:04:43 +0530

    Modify styles + Mock data setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    Todos
+  },
+  todos: [
+    { id: 1, title: "Setup vue cli", done: true },
+    { id: 2, title: "App scaffholding", done: true },
+    { id: 3, title: "Component generation", done: false }
+  ]
+};
 </script>
-
+ 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  line-height: 1.5;
 }
 </style>
