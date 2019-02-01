8aab3764e4653b5373e45d53ed1a52b1c6c7e346
Amal Shehu <amalshehu@gmail.com>
2019-02-02 03:26:32 +0530

    Add todo component

---
+++ src/components/Todo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>Todo Item</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo"
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: floralwhite;
+  padding: 10px;
+  border-bottom: 1px #dddddd dotted;
+}
+.is-complete {
+  text-decoration-line: line-through;
+}
+.del {
+  background: red;
+  color: #dddddd;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>
